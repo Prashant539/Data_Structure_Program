58733ba294e13c7782a838f40a79ef3016700518
Prashant <prashant.chaurasiya.cp@gmail.com>
2023-01-01 22:49:08 +0530

    [Prashant]Add & Search: Unordered List

---
+++ UnOrderedList/UnOrderedList/Linkedlist.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+
+namespace UnOrderedList
+{
+    public class Linkedlist<T>
+    {
+        public Node<T> head;
+        
+
+        public void Add(T data)
+        {
+            Node<T> node = new Node<T>(data);
+
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node<T> temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+        }
+
+
+        public void Display()
+        {
+            Node<T> temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("LinkedList is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+        public void Search(T data)
+        {
+            Node<T> temp = this.head;
+            while (temp != null)
+            {
+                if (temp.data.Equals(data))
+                {
+                    Console.WriteLine("This Word {0} is found ", temp.data);
+
+                    return;
+                }
+                temp = temp.next;
+            }
+            if (temp == null)
+            {
+                Console.WriteLine("{0} This Word is not found", data);
+                Add(data);
+            }
+        }
+    }
+}
+
+       
+
+        
+
+    
+
+
+
+
+    
+    
+
+
+    
+
+
+
+
---
+++ UnOrderedList/UnOrderedList/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using UnOrderedList;
+
+namespace UnorderedList
+{
+    public class Program
+    {
+       public static void Main(string[] args)
+        {
+            Console.WriteLine("Data Structure Programs!");
+            Linkedlist<string> list= new Linkedlist<string>();
+            //Linkedlist<string> list = new Linkedlist<string>();
+            string txt = "My name is Prashant from gorakhpur";
+            string[] textList = txt.Split(" ");
+            foreach(var Data in textList)
+               
+            {
+                
+                list.Add(Data);
+                ///Console.WriteLine(Data)
+                list.Search("My");
+               
+            }
+            list.Display();
+
+
+        }
+    }
+}
